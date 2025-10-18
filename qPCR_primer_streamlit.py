4838250caec0971bec009735eb715fca1843e429
chuang-DH <benalu85853@gmail.com>
2025-10-19 01:28:26 +0800

    Change min_match from 1 to 2 in is_self_complementary
    Increase minimum match requirement for self-complementary check.

--- qPCR_primer_streamlit.py
+++ qPCR_primer_streamlit.py
@@ -40,7 +40,7 @@ def reverse_complement(seq):
     comp = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
     return ''.join(comp.get(base, base) for base in reversed(seq))
 
-def is_self_complementary(seq, min_match=1):
+def is_self_complementary(seq, min_match=2):
     seq = sanitize_seq(seq)
     length = len(seq)
     if length < min_match + 1:
